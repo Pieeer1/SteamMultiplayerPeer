d9fa0670b8dd7fa5a9aa0f6d79da11400f3075cd
Pieeer1 <mason.adams@msn.com>
2024-03-04 13:25:52 -0800

    double intake

--- addons/GodotVoipNet/Scripts/VoiceInstance.cs
+++ addons/GodotVoipNet/Scripts/VoiceInstance.cs
@@ -30,7 +30,7 @@ public partial class VoiceInstance : Node
     [Export]
     public bool ShouldListen { get; set; } = false;
     [Export]
-    public double InputThreshold { get; set; } = 0.005f;
+    public double InputThreshold { get; set; } = 0.01f;
 
     public event EventHandler<VoiceDataEventArgs>? ReceivedVoiceData;
     public event EventHandler<VoiceDataEventArgs>? SentVoiceData;
