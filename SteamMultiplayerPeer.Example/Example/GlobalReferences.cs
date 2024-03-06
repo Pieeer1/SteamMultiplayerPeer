e767631f59527f4c8722186f6c505241818693a7
Pieeer1 <mason.adams@msn.com>
2024-03-05 20:06:26 -0800

    this shit is so broken

--- SteamMultiplayerPeer.Example/Example/GlobalReferences.cs
+++ SteamMultiplayerPeer.Example/Example/GlobalReferences.cs
@@ -4,6 +4,6 @@ using Steam;
 namespace SteamMultiplayerPeer.Example;
 internal static class GlobalReferences // is this bad practice? idk its an example project
 {
-    public static SteamManager SteamManager(this Node node) => node.GetTree().Root.GetNode<SteamManager>("Startup/SteamManager");
+    public static SteamManager SteamManager(this Node node) => node.GetTree().Root.GetNode<Startup>("Startup").SteamManager;
 
 }
--- SteamMultiplayerPeer.Example/Example/Startup.cs
+++ SteamMultiplayerPeer.Example/Example/Startup.cs
@@ -1,6 +1,12 @@
 using Godot;
 using System;
-
+using Steam;
 public partial class Startup : Node3D
 {
+    public SteamManager SteamManager { get; } = new SteamManager();
+
+    public override void _Ready()
+    {
+        AddChild(SteamManager);    
+    }
 }
