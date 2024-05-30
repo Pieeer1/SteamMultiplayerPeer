fb74b0c7eb1a83b3bb7a9a250e55b27aec1585b9
Pieeer1 <mason.adams@msn.com>
2024-05-29 19:10:26 -0700

    initial ability to run tests.

---
+++ Tests/TempTest.cs
@@ -0,0 +1,12 @@
+﻿using GdUnit4;
+
+namespace SteamMultiplayerPeer.Tests;
+[TestSuite]
+public class TempTest
+{
+    [TestCase]
+    public void Test1()
+    {
+        Assertions.AssertThat(true).IsTrue();
+    }
+}
